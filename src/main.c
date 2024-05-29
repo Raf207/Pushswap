c7e8f2dd1d47de1422dea6f947aabafd5ba903b0
Rafael Nascimento Cardoso <rafnasci@fu-r6-p2.s19.be>
2024-05-29 13:07:41 +0200

    pas bon

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rafnasci <rafnasci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 16:37:16 by rafnasci          #+#    #+#             */
-/*   Updated: 2024/05/23 16:21:57 by rafnasci         ###   ########.fr       */
+/*   Updated: 2024/05/27 14:19:11 by rafnasci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ int	main (int ac, char **av)
 		if(ft_stack_len(a) == 2)
 			sa(a);
 		else if (ft_stack_len(a) == 3)
-			ft
+			ft  
 				
 	}
 	if (ac == 2)
